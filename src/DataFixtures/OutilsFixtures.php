eba72395ff1446316f372e319adb4c7811cfa781
chris76550 <chrislas76@gmail.com>
2020-06-06 10:33:10 +0200

    Test 3

--- src/DataFixtures/OutilsFixtures.php
+++ src/DataFixtures/OutilsFixtures.php
@@ -2,7 +2,7 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\User;
+use App\Entity\Outils;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 
@@ -11,21 +11,16 @@ class UserFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         for ($i = 1; $i <= 20; $i++) {
-            $user = new User();
-            $user->setNom('bob' . $i)
-                ->setPrenom('truc' . $i)
-                ->setEmail("azerty@azerty$i.com")
-                ->setPassword("azerty")
-                ->setAdresse("adresse$i")
-                ->setTelephone($i)
-                ->setRoles(["ROLE_USER"]);
+            $outil = new Outils();
+            $outil->setOutil('perceuse' . $i)
+                ->setDescription('belle perceuse' . $i)
+                ->setImage("image" . $i)
+                ->setPrix($i+10);
 
-            $manager->persist($user);
-
+            $manager->persist($outil);
 
             // $product = new Product();
             // $manager->persist($product);
-
 
         }
 

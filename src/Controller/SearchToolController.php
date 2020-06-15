af6d6f1e1348e6ce324021b728375efd5270f3db
chris76550 <chrislas76@gmail.com>
2020-06-15 16:01:47 +0200

    Le 15 juin, modifs des intitulés des champs et redirection de 'Je recherche un outil' de l'accueil vers le 'search_tool'.

--- src/Controller/SearchToolController.php
+++ src/Controller/SearchToolController.php
@@ -1,16 +1,21 @@
 <?php
 
 namespace App\Controller;
+use App\Entity\Outils;
+use App\Entity\User;
+use App\Entity\Categories;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SearchToolController extends AbstractController
 {
     /**
      * @Route("/search/tool", name="search_tool")
      */
-    public function index()
+    public function index(Request $request): Response
     {
         return $this->render('search_tool/index.html.twig', [
             'controller_name' => 'SearchToolController',
